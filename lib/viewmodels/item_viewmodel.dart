b70bd19bd36349a914a11598010b950fd0828a23
Alisaad525 <Ali-ksaad@hotmail.com>
2024-09-22 18:22:21 +0300

    search bar fixed

--- lib/viewmodels/item_viewmodel.dart
+++ lib/viewmodels/item_viewmodel.dart
@@ -2,13 +2,24 @@ import '../models/item.dart';
 
 class ItemViewModel {
   final List<Item> items = [
-    Item(imageUrl: 'assets/1.jpg', title: 'Item 1', price: 29.99, rating: 4.5, discount: 5.0),
-    Item(imageUrl: 'assets/2.jpg', title: 'Item 2', price: 49.99, rating: 4.0, discount: 20.0),
-    Item(imageUrl: 'assets/3.jpg', title: 'Item 3', price: 19.99, rating: 3.5, discount: 10.0),
+    Item(imageUrl: 'assets/1.jpg', title: 'spatula', price: 29.99, rating: 0, discount: 5.0),
+    Item(imageUrl: 'assets/2.jpg', title: 'Spatula', price: 49.99, rating: 0, discount: 20.0),
+    Item(imageUrl: 'assets/3.jpg', title: 'Item 3', price: 19.99, rating: 0, discount: 10.0),
+    Item(imageUrl: 'assets/4.jpg', title: 'Item 4', price: 59.99, rating: 0, discount: 15.0),
+    Item(imageUrl: 'assets/4.jpg', title: 'Item 4', price: 59.99, rating: 0, discount: 15.0),
+    Item(imageUrl: 'assets/4.jpg', title: 'Item 4', price: 59.99, rating: 0, discount: 15.0),
+    Item(imageUrl: 'assets/4.jpg', title: 'Item 4', price: 59.99, rating: 0, discount: 15.0),
     Item(imageUrl: 'assets/4.jpg', title: 'Item 4', price: 59.99, rating: 5.0, discount: 15.0),
+
+
   ];
 
   List<Item> searchItems(String query) {
-    return items.where((item) => item.title.toLowerCase().contains(query.toLowerCase())).toList();
+    String formattedQuery = query.toLowerCase().replaceAll(' ', '');
+    return items.where((item) {
+      String formattedTitle = item.title.toLowerCase().replaceAll(' ', '');
+      return formattedTitle.contains(formattedQuery);
+    }).toList();
   }
+
 }
